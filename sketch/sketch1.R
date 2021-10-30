f51fcccf5eb762f1d87491abe8543004f3e8d2e6
Daniel E. Weeks <weeks@pitt.edu>
2021-10-30 09:15:31 -0400

    Trying out the 'sketch' library, an R interface into p5js

---
+++ sketch/sketch1.R
@@ -0,0 +1,21 @@
+#! config(deparsers = default_2_deparsers())
+#! load_library("p5")
+
+# Setup
+WIDTH <- 400
+HEIGHT <- 400
+DIAMETER <- 10
+GRID_SIZE <- 40
+
+x_grid <- seq(0, WIDTH, step = DIAMETER)
+y_grid <- seq(0, HEIGHT, step = DIAMETER)
+
+setup <- function() {
+    createCanvas(WIDTH, HEIGHT)
+    for (x in x_grid) {
+        for (y in y_grid) {
+            circle(x, y, DIAMETER)
+        }
+    }
+    NULL
+}
---
+++ sketch/sketch2save.R
@@ -0,0 +1,30 @@
+#! config(deparsers = default_2_deparsers())
+#! load_library("p5")
+
+# Setup
+WIDTH <- 400
+HEIGHT <- 400
+DIAMETER <- 10
+GRID_SIZE <- 40
+
+x_grid <- seq(0, WIDTH, step = DIAMETER)
+y_grid <- seq(0, HEIGHT, step = DIAMETER)
+
+setup <- function() {
+    cnv = createCanvas(WIDTH, HEIGHT)
+    background('rgb(255,255,255)')
+    for (x in x_grid) {
+        for (y in y_grid) {
+            # Uniform distribution for RGB color
+            rgb_color = runif(3, 0, 255)
+            fill(rgb_color)    # fill color
+            stroke(rgb_color)  # border color
+
+            # Square of normal distribution for the diameter
+            d <- DIAMETER * rnorm(1, 0, 0.4)^2
+            circle(x, y, d)
+        }
+    }
+    save(cnv, "sketch2.png")
+    NULL
+}
